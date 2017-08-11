6c250324555ca9bd8a226c47005146ffb8932a9b
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-11 11:27:01 -0400

    It shows up now, but now to fix it up and then get it to function.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const app = express()
+const pgPromise = require('pg-promise')()
+const database = pgPromise({ database: 'DinoDB' })
+const query = 'SELECT * FROM dinosaursdb'
+const queryuser = 'SELECT * FROM dinosaursdb WHERE id = ${id}'
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
@@ -11,58 +15,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  let allDinos = [
-    {
-      id: 1,
-      name: 'Tyrannosaurus Rex',
-      color: ['Green', 'Tan'],
-      length: '42 feet',
-      weight: '7 tons',
-      habitat: 'Temperate Coniferous Forest'
-    },
-    {
-      id: 2,
-      name: 'Velociraptor',
-      color: ['Green', 'Orange'],
-      length: '6 feet',
-      weight: '33 lbs',
-      habitat: 'Desert'
-    },
-    {
-      id: 3,
-      name: 'Triceratops',
-      color: ['Tan', 'Brown'],
-      length: '26 feet',
-      weight: '12 tons',
-      habitat: 'Temperate Coniferous Forest'
-    },
-    {
-      id: 4,
-      name: 'Brachiosaurus',
-      color: ['Gray', 'Tan'],
-      length: '85 feet',
-      weight: '50 tons',
-      habitat: 'Temperate Deciduous Forest'
-    },
-    {
-      id: 5,
-      name: 'Stegosaurus',
-      color: ['Green', 'Orange', 'Tan'],
-      length: '30 ft',
-      weight: '5 tons',
-      habitat: 'Grassland'
-    },
-    {
-      id: 6,
-      name: 'Iguanodon',
-      color: ['Brown', 'Tan'],
-      length: '33 feet',
-      weight: '3.5 tons',
-      habitat: 'Temperate Deciduous Forest'
-    }
-  ]
-  res.render('index', allDinos)
-  console.log(allDinos)
+  database.any(query).then(rows => {
+    res.render('index', { rows })
+  })
 })
 
 app.get('/api/dinosaurs/:id', (req, res) => {
---
+++ schema.js
@@ -0,0 +1,107 @@
+CREATE TABLE dinosaursdb (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+imageurl VARCHAR(200) NOT NULL,
+length VARCHAR(100) NOT NULL,
+weight VARCHAR(100) NOT NULL,
+habitat VARCHAR(100) NOT NULL);
+
+INSERT INTO dinosaursdb (
+name,
+imageurl,
+length,
+weight,
+habitat)
+VALUES (
+'Tyrannosaurus Rex',
+'http://www.sciencekids.co.nz/images/pictures/dinosaurs/tyrannosaurusrex/trexroar.jpg',
+'42 feet',
+'7 tons',
+'Temperate Coniferous Forest'
+),
+(
+'Velociraptor',
+'http://www.sciencekids.co.nz/images/pictures/dinosaurs/velociraptor.jpg',
+'6 feet',
+'33 lbs',
+'Desert'
+),
+(
+'Triceratops',
+'http://www.sciencekids.co.nz/images/pictures/dinosaurs/triceratops/triceratopsfacesketch.JPG',
+'26 feet',
+'12 tons',
+'Temperate Coniferous Forest'
+),
+(
+'Brachiosaurus',
+'http://www.sciencekids.co.nz/images/pictures/dinosaurs/brachiosaurusdrawing.jpg',
+'85 feet',
+'50 tons',
+'Temperate Deciduous Forest'
+),
+(
+'Stegosaurus',
+'http://www.sciencekids.co.nz/images/pictures/dinosaurs/stegosaurus/stegosaurusillustration.jpg',
+'30 ft',
+'5 tons',
+'Grassland'
+),
+(
+'Iguanodon',
+'http://www.sciencekids.co.nz/images/experiments/iguanodon170.jpg',
+'33 feet',
+'3.5 tons',
+'Temperate Deciduous Forest'
+);
+
+// let allDinos = [
+//   {
+//     id: 1,
+//     name: 'Tyrannosaurus Rex',
+//     color: ['Green', 'Tan'],
+//     length: '42 feet',
+//     weight: '7 tons',
+//     habitat: 'Temperate Coniferous Forest'
+//   },
+//   {
+//     id: 2,
+//     name: 'Velociraptor',
+//     color: ['Green', 'Orange'],
+//     length: '6 feet',
+//     weight: '33 lbs',
+//     habitat: 'Desert'
+//   },
+//   {
+//     id: 3,
+//     name: 'Triceratops',
+//     color: ['Tan', 'Brown'],
+//     length: '26 feet',
+//     weight: '12 tons',
+//     habitat: 'Temperate Coniferous Forest'
+//   },
+//   {
+//     id: 4,
+//     name: 'Brachiosaurus',
+//     color: ['Gray', 'Tan'],
+//     length: '85 feet',
+//     weight: '50 tons',
+//     habitat: 'Temperate Deciduous Forest'
+//   },
+//   {
+//     id: 5,
+//     name: 'Stegosaurus',
+//     color: ['Green', 'Orange', 'Tan'],
+//     length: '30 ft',
+//     weight: '5 tons',
+//     habitat: 'Grassland'
+//   },
+//   {
+//     id: 6,
+//     name: 'Iguanodon',
+//     color: ['Brown', 'Tan'],
+//     length: '33 feet',
+//     weight: '3.5 tons',
+//     habitat: 'Temperate Deciduous Forest'
+//   }
+// ]
