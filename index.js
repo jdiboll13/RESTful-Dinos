b1c0a548dc2b6f78245738a2901486423c578459
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-11 09:38:38 -0400

    It's not rendering. I ahve work to do.

--- index.js
+++ index.js
@@ -11,58 +11,59 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
+  let allDinos = [
+    {
+      id: 1,
+      name: 'Tyrannosaurus Rex',
+      color: ['Green', 'Tan'],
+      length: '42 feet',
+      weight: '7 tons',
+      habitat: 'Temperate Coniferous Forest'
+    },
+    {
+      id: 2,
+      name: 'Velociraptor',
+      color: ['Green', 'Orange'],
+      length: '6 feet',
+      weight: '33 lbs',
+      habitat: 'Desert'
+    },
+    {
+      id: 3,
+      name: 'Triceratops',
+      color: ['Tan', 'Brown'],
+      length: '26 feet',
+      weight: '12 tons',
+      habitat: 'Temperate Coniferous Forest'
+    },
+    {
+      id: 4,
+      name: 'Brachiosaurus',
+      color: ['Gray', 'Tan'],
+      length: '85 feet',
+      weight: '50 tons',
+      habitat: 'Temperate Deciduous Forest'
+    },
+    {
+      id: 5,
+      name: 'Stegosaurus',
+      color: ['Green', 'Orange', 'Tan'],
+      length: '30 ft',
+      weight: '5 tons',
+      habitat: 'Grassland'
+    },
+    {
+      id: 6,
+      name: 'Iguanodon',
+      color: ['Brown', 'Tan'],
+      length: '33 feet',
+      weight: '3.5 tons',
+      habitat: 'Temperate Deciduous Forest'
+    }
+  ]
   res.render('index', allDinos)
+  console.log(allDinos)
 })
-let allDinos = [
-  {
-    id: 1,
-    name: 'Tyrannosaurus Rex',
-    color: ['Green', 'Tan'],
-    length: '42 feet',
-    weight: '7 tons',
-    habitat: 'Temperate Coniferous Forest'
-  },
-  {
-    id: 2,
-    name: 'Velociraptor',
-    color: ['Green', 'Orange'],
-    length: '6 feet',
-    weight: '33 lbs',
-    habitat: 'Desert'
-  },
-  {
-    id: 3,
-    name: 'Triceratops',
-    color: ['Tan', 'Brown'],
-    length: '26 feet',
-    weight: '12 tons',
-    habitat: 'Temperate Coniferous Forest'
-  },
-  {
-    id: 4,
-    name: 'Brachiosaurus',
-    color: ['Gray', 'Tan'],
-    length: '85 feet',
-    weight: '50 tons',
-    habitat: 'Temperate Deciduous Forest'
-  },
-  {
-    id: 5,
-    name: 'Stegosaurus',
-    color: ['Green', 'Orange', 'Tan'],
-    length: '30 ft',
-    weight: '5 tons',
-    habitat: 'Grassland'
-  },
-  {
-    id: 6,
-    name: 'Iguanodon',
-    color: ['Brown', 'Tan'],
-    length: '33 feet',
-    weight: '3.5 tons',
-    habitat: 'Temperate Deciduous Forest'
-  }
-]
 
 app.get('/api/dinosaurs/:id', (req, res) => {
   const dinoId = parseInt(req.params.id)
